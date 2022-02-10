072005c4933ee930c8e8fdb7f7c4bc3f8b5d11ad
Ezequiel Rangel <chekelinho@gmail.com>
2022-02-10 15:13:59 -0700

    Add props to card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,15 +8,32 @@ import "./CreditCard.css"
 //   cctype: "mastercard",
 // }
 
+const images = {
+  "amex": "./amex.png",
+  "discover": "./discover.png",
+  "jcb": "./jcb.png",
+  "visa": "./visa.png",
+  "visa-electron": "./visa-electron.png",
+  "mastercard": "./mastercard.png"
+};
+
 // 1. Crear un stateless component
-const Card = () => {
+const Card = (props) => {
+  // 3. Añadir props
+  const { title = "", ccending = "", ccholder = "", ccexp = "", cctype = ""} = props;
+
   return (
     <div className="Card">
       <header className="Card__head">
-        <h6 className="Card__title">Tarjeta de nómina</h6>
+        <h6 className="Card__title">{title}</h6>
       </header>
       <div className="Card__type">
-        <img src="./mastercard.png" alt="Tarjeta Mastercard" width={53} height={32} />
+        <img
+          src={images[cctype]}
+          alt="Imagen de la tarjeta"
+          width={53}
+          height={32}
+        />
       </div>
       <div className="Card__number">
         <span className="Card__number-mask">
@@ -24,16 +41,16 @@ const Card = () => {
           <span>&bull;&bull;&bull;&bull;</span>
           <span>&bull;&bull;&bull;&bull;</span>
         </span>
-        <span>4398</span>
+        <span>{ccending}</span>
       </div>
       <div className="Card__info">
         <div className="Card__block">
           <h6 className="Card__title">Titular de la tarjeta</h6>
-          <span>Ezequiel Rangel Ibarra</span>
+          <span>{ccholder}</span>
         </div>
         <div className="Card__block">
           <h6 className="Card__title">Vence hasta</h6>
-          <span>08/2024</span>
+          <span>{ccexp}</span>
         </div>
       </div>
     </div>
