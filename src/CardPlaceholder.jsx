761ee188339a11780f610b8592ce044e914ee76a
Ezequiel Rangel <chekelinho@gmail.com>
2022-02-10 12:51:55 -0700

    Placeholder for card component

---
+++ src/CardPlaceholder.jsx
@@ -0,0 +1,13 @@
+import "./CardPlaceholder.css";
+
+function CardPlaceholder(){
+  return (
+    <div className="CardPlaceholder">
+      <small className="CardPlaceholder__copy">
+        Aquí habrá un componente para la tarjeta de crédito
+      </small>
+    </div>
+  )
+}
+
+export default CardPlaceholder;
